96070860042df92db68c87f5c97b6c196f995c2a
Zvonimir <zvonimirplivelic91@gmail.com>
2021-11-29 22:31:44 +0100

    Created expandable card in recycler view

--- app/src/main/java/com/zvonimirplivelic/parkingzg/ui/adapter/VehicleListAdapter.kt
+++ app/src/main/java/com/zvonimirplivelic/parkingzg/ui/adapter/VehicleListAdapter.kt
@@ -1,10 +1,15 @@
 package com.zvonimirplivelic.parkingzg.ui.adapter
 
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.zvonimirplivelic.parkingzg.R
@@ -25,21 +30,34 @@ class VehicleListAdapter : RecyclerView.Adapter<VehicleListAdapter.VehicleViewHo
 
     override fun onBindViewHolder(holder: VehicleViewHolder, position: Int) {
         val currentVehicle = vehicleList[position]
+
+        var vehicleInfoCard = holder.itemView.findViewById<CardView>(R.id.vehicle_list_item)
+        var vehicleInfoCardExpandable = holder.itemView.findViewById<LinearLayout>(R.id.expandable_pay_buttons_layout)
+
         var tvVehicleModel = holder.itemView.findViewById<TextView>(R.id.tv_vehicle_model)
         var tvVehicleManufacturer =
             holder.itemView.findViewById<TextView>(R.id.tv_vehicle_manufacturer)
         var tvVehicleRegistrationNumber =
             holder.itemView.findViewById<TextView>(R.id.tv_vehicle_registration_number)
+
         var ivVehicleInfo = holder.itemView.findViewById<ImageView>(R.id.iv_vehicle_info)
+        var btnZoneOne = holder.itemView.findViewById<Button>(R.id.btn_zone_one)
+        var btnZoneTwo = holder.itemView.findViewById<Button>(R.id.btn_zone_two)
+        var btnZoneThree = holder.itemView.findViewById<Button>(R.id.btn_zone_three)
 
         tvVehicleModel.text = currentVehicle.vehicleModel
         tvVehicleManufacturer.text = currentVehicle.vehicleManufacturer
         tvVehicleRegistrationNumber.text = currentVehicle.vehicleRegistrationNumber
+
         ivVehicleInfo.setOnClickListener {
             val action =
                 VehicleListFragmentDirections
                     .actionVehicleListFragmentToVehicleInfoFragment(currentVehicle)
             it.findNavController().navigate(action)
+        }
+
+        vehicleInfoCard.setOnClickListener {
+            expandCardView(vehicleInfoCardExpandable, vehicleInfoCard)
         }
     }
 
@@ -48,5 +66,18 @@ class VehicleListAdapter : RecyclerView.Adapter<VehicleListAdapter.VehicleViewHo
     fun setData(vehicles: List<Vehicle>) {
         this.vehicleList = vehicles
         notifyDataSetChanged()
+    }
+
+    private fun expandCardView(
+        expandableLayout: LinearLayout,
+        expandableCard: CardView
+    ) {
+        if (expandableLayout.visibility == View.GONE) {
+            TransitionManager.beginDelayedTransition(expandableCard, AutoTransition())
+            expandableLayout.visibility = View.VISIBLE
+        } else {
+            TransitionManager.beginDelayedTransition(expandableCard, AutoTransition())
+            expandableLayout.visibility = View.GONE
+        }
     }
 }
